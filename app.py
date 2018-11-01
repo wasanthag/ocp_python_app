358e7a2ade4479e18ef6e418c54a7736d1e1e45e
Wasantha Gamage <whewawal@redhat.com>
2018-10-31 21:06:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print ("Hello world")
+import SimpleHTTPServer
+import SocketServer
+
+PORT = 8080
+
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+print "serving at port", PORT
+httpd.serve_forever()
