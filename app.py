66fb5c0fad1a5e142983f710c3287fb42d12822c
Wasantha Gamage <whewawal@redhat.com>
2018-10-31 20:10:46 -0400

    add brackets

--- app.py
+++ app.py
@@ -20,11 +20,11 @@ try:
 	#Create a web server and define the handler to manage the
 	#incoming request
 	server = HTTPServer(('', PORT_NUMBER), myHandler)
-	print 'Started httpserver on port 8080'
+	print ('Started httpserver on port 8080')
 	
 	#Wait forever for incoming htto requests
 	server.serve_forever()
 
 except KeyboardInterrupt:
-	print '^C received, shutting down the web server'
+	print ('^C received, shutting down the web server')
 	server.socket.close()
