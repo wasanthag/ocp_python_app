78e1bdd82a025399ee32b77cc807e0727c287f87
Wasantha Gamage <whewawal@redhat.com>
2018-10-31 21:24:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
+import http.server
+import socketserver
 
+PORT = 8000
 
-class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+Handler = http.server.SimpleHTTPRequestHandler
 
-    def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b'Hello, world!')
-
-
-httpd = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
-httpd.serve_forever()
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
