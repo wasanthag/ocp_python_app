4d5205c4cf73684318c6d4693ff53f6ea92a90eb
Wasantha Gamage <whewawal@redhat.com>
2018-10-31 21:55:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,52 +1,15 @@
-import time
-from http.server import BaseHTTPRequestHandler, HTTPServer
 
-HOST_NAME = 'localhost'
-PORT_NUMBER = 8080
+import http.server
+import socketserver
+from http import HTTPStatus
 
 
-class MyHandler(BaseHTTPRequestHandler):
-    def do_HEAD(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-
+class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        paths = {
-            '/foo': {'status': 200},
-            '/bar': {'status': 302},
-            '/baz': {'status': 404},
-            '/qux': {'status': 500}
-        }
-
-        if self.path in paths:
-            self.respond(paths[self.path])
-        else:
-            self.respond({'status': 500})
-
-    def handle_http(self, status_code, path):
-        self.send_response(status_code)
-        self.send_header('Content-type', 'text/html')
+        self.send_response(HTTPStatus.OK)
         self.end_headers()
-        content = '''
-        <html><head><title>S2I test</title></head>
-        <body><p>This is a ocp s2i test.</p>
-        <p>You accessed path: {}</p>
-        </body></html>
-        '''.format(path)
-        return bytes(content, 'UTF-8')
+        self.wfile.write(b'Hello world')
 
-    def respond(self, opts):
-        response = self.handle_http(opts['status'], self.path)
-        self.wfile.write(response)
 
-if __name__ == '__main__':
-    server_class = HTTPServer
-    httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
-    print(time.asctime(), 'Server Starts - %s:%s' % (HOST_NAME, PORT_NUMBER))
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        pass
-    httpd.server_close()
-print(time.asctime(), 'Server Stops - %s:%s' % (HOST_NAME, PORT_NUMBER))
+httpd = socketserver.TCPServer(('', 8080), Handler)
+httpd.serve_forever()
